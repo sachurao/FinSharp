a1a105d637c56ddf1856725d6792daf85f377f9f
Sachin Rao <dev@sachinrao.co.uk>
2012-04-20 23:08:18 +0100

    TK-01001 creating the desktop application

---
+++ src/main/net/Desktop/Irrefutable.FinSharp.Desktop/Irrefutable.FinSharp.Desktop.Common/DesktopDefaults.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Irrefutable.FinSharp.Desktop.Common
+{
+    public static class DesktopDefaults
+    {
+        public const string DESKTOP_REGION_MAIN = "MainRegion";
+        public const string DESKTOP_REGION_SUMMARY = "SummaryRegion";
+        public const string DESKTOP_REGION_DETAILS = "DetailsRegion";
+        public const string DESKTOP_REGION_SECONDARY = "SecondaryRegion";
+        public const string RELATIVE_PATH_MODULES = @".\Modules";
+
+    }
+}
