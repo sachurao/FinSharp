7db4ee61107d2ad1b166e42f8ba15f90577e3779
Sachin Rao <dev@sachinrao.co.uk>
2012-07-04 00:54:57 +0100

    TK-01005 Adding BondAnalysis

---
+++ Code/NET/DesktopModules/BondAnalysis/BondCalculator.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace StreamCipher.FinSharp.BondAnalysisModule
+{
+    /// <summary>
+    /// Interaction logic for BondCalculator.xaml
+    /// </summary>
+    public partial class BondCalculator : UserControl
+    {
+        public BondCalculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Code/NET/DesktopModules/BondAnalysis/ModuleDefinition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Irrefutable.FinSharp.Desktop.Common;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace StreamCipher.FinSharp.BondAnalysisModule
+{
+    [Module(ModuleName = "Bond Analysis Tools")]
+    public class ModuleDefinition:IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        public ModuleDefinition(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+
+        #region Implementation of IModule
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion(DesktopDefaults.WindowRegionWorkspace, typeof(BondCalculator));
+        }
+
+        #endregion
+    }
+}
